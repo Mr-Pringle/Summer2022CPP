1b92b253037b46a8569bc3fe6f697022715d4109
Mr-Pringle <71740126+Mr-Pringle@users.noreply.github.com>
2022-08-17 11:44:59 -0400

    Update (working on prefabs)

--- Assets/Sprites/Player/PlayerController.cs
+++ Assets/Sprites/Player/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
-[RequiredComponent(typeof(Shoot))]
+[RequireComponent(typeof(Shoot))]
 
 
 public class PlayerController : MonoBehaviour
